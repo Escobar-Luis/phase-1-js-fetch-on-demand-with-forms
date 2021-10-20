7c3bd2ccca30e0f694cab2229c4045b9fb11382a
Luis Escobar <lae311@stern.nyu.edu>
2021-10-20 14:43:32 -0400

    commit tree 700859f655c21cf3abe9eb39e6fe19b3c3626c9d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form')
+
+    inputForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID')
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(function toJson (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                const title = document.querySelector('section#movieDetails h4')
+                const summary = document.querySelector('section#movieDetails p')
+
+                title.innerHTML = data.title
+                summary.innerHTML = data.summary
+                console.log(title, summary)
+            })
+    })
 }
 
+
 document.addEventListener('DOMContentLoaded', init);
+
+
+
